fd23bc04d4f537cf383d54305988db2a512d13f0
PAULO VICTOR ALMEIDA DE OLIVEIRA <vitoralmeide@gmail.com>
2025-03-06 18:18:46 -0300

    novo layout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 
+// Animação do título "DESENVOLVEDOR FRONT-END JUNIOR"
+document.addEventListener("DOMContentLoaded", () => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+
+
+    gsap.to(".caixa", {
+        y: -1000, // Move 300px para a direita
+        x:  1000,
+        duration: 10,
+        repeat: -1, // Repetição infinita
+        yoyo: false, // Volta para a posição original
+        ease: "power1.inOut" // Suavização do movimento
+    });
+    
+
+
+    gsap.from(".card", {
+        opacity: 0,
+        y: -100, // Move para cima no início (vindo de baixo)
+        duration: 3, // Duração mais curta para uma animação mais suave
+        scrollTrigger: {
+            trigger: ".card", // Elemento que ativa a animação
+            start: "top 80%", // Começa quando o topo do elemento atinge 80% da tela
+            toggleActions: "play none none none", // Animação roda apenas uma vez
+            markers: false, // Desative os marcadores para produção
+        }
+    });
+
+});
