4b0461eb184e202020e06973bccca3cebf911bfb
PAULO VICTOR ALMEIDA DE OLIVEIRA <vitoralmeide@gmail.com>
2025-03-09 04:49:22 -0300

    test

--- js/script.js
+++ js/script.js
@@ -5,11 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger);
 
 
-
     gsap.to(".caixa", {
-        y: -1000, // Move 300px para a direita
-        x:  1000,
-        duration: 10,
+        y: -350, // Move 300px para a direita
+        x:  350,
+        duration: 5,
         repeat: -1, // Repetição infinita
         yoyo: false, // Volta para a posição original
         ease: "power1.inOut" // Suavização do movimento
@@ -17,16 +16,28 @@ document.addEventListener("DOMContentLoaded", () => {
     
 
 
-    gsap.from(".card", {
+    gsap.from(".card-exp", {
         opacity: 0,
-        y: -100, // Move para cima no início (vindo de baixo)
+        y: -400, // Move para cima no início (vindo de baixo)
         duration: 3, // Duração mais curta para uma animação mais suave
+        stagger: 2, // Atraso entre cada animação
+        immediateRender: false,
         scrollTrigger: {
-            trigger: ".card", // Elemento que ativa a animação
-            start: "top 80%", // Começa quando o topo do elemento atinge 80% da tela
-            toggleActions: "play none none none", // Animação roda apenas uma vez
-            markers: false, // Desative os marcadores para produção
+            trigger: ".card-exp", // Elemento que ativa a animação
+            start: "top 90%", 
+            end: "bottom top" ,// Começa quando o topo do elemento atinge 80% da tela
+            toggleActions: "play reset play reset", // Animação roda apenas uma vez
+            markers: true, // Desative os marcadores para produção
         }
     });
 
+    
+    gsap.to(".card", {
+        y: 13, // Move o elemento 50px para cima
+        duration: 1, // Duração da animação (1 segundo)
+        repeat: -1, // Repete infinitamente
+        yoyo: true, // Faz a animação ir e voltar
+        ease: "power1.inOut", // Efeito de easing suave
+      
+    });
 });
